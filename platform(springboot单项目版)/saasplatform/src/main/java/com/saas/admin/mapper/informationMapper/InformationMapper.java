68c4e8d439a57faf9722fa52e0a6b7011a852773
VincentCeng <32964935+VincentCeng@users.noreply.github.com>
2018-07-08 00:05:01 +0800

    增加资讯后台分页查询

--- platform(springboot单项目版)/saasplatform/src/main/java/com/saas/admin/mapper/informationMapper/InformationMapper.java
+++ platform(springboot单项目版)/saasplatform/src/main/java/com/saas/admin/mapper/informationMapper/InformationMapper.java
@@ -1,6 +1,10 @@
 package com.saas.admin.mapper.informationMapper;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.github.pagehelper.Page;
 import com.saas.admin.model.information.Information;
+import com.saas.mybatis.plugins.BaseConditionVO;
 
 public interface InformationMapper {
     int deleteByPrimaryKey(Integer id);
@@ -16,4 +20,6 @@ public interface InformationMapper {
     int updateByPrimaryKeyWithBLOBs(Information record);
 
     int updateByPrimaryKey(Information record);
+    
+    Page<Information> findInformationByVo(@Param("vo")BaseConditionVO vo);
 }
--- platform(springboot单项目版)/saasplatform/src/test/java/com/saas/platform/saasplatform/SaasplatformApplicationTests.java
+++ platform(springboot单项目版)/saasplatform/src/test/java/com/saas/platform/saasplatform/SaasplatformApplicationTests.java
@@ -2,16 +2,44 @@ package com.saas.platform.saasplatform;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.github.pagehelper.Page;
+import com.saas.admin.mapper.UserMapper;
+import com.saas.admin.model.User;
+import com.saas.mybatis.plugins.BaseConditionVO;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SaasplatformApplicationTests {
 
+	
+	@Autowired
+	UserMapper userMapper;
+	
 	@Test
 	public void contextLoads() {
 		System.out.println("dasd");
 	}
+	
+	@Test
+	private void test2() {
+		/** 
+		 * @author 刘曾 
+		 * @version 2018年7月7日 下午9:46:57 
+		 */
+		System.out.println("进入。。。");
+		BaseConditionVO vo=new BaseConditionVO();
+		vo.setCount(1);
+		vo.setPage(1);
+		Page<User> list=userMapper.findByVo(vo);
+		for (User user : list) {
+			System.out.println(user);
+		}
+		
+	}
+	
 
 }
